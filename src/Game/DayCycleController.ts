abbef94fda95328e18624421f77acf87f51a40e5
Mizab <mizabmansari@gmail.com>
2024-09-06 17:02:03 +0530

    Added basic controller

---
+++ src/Game/DayCycleController.ts
@@ -0,0 +1,36 @@
+import { _, execute, MCFunction, time, title } from "sandstone";
+import { TIME_TICKS } from "../Constants";
+import { daysPassed, timeOfDay } from "../Tick";
+
+const dayNumberArr = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// Speed up the day, called by Tick function
+export const speedUpDayClock = () => {
+  time.add(TIME_TICKS);
+};
+
+// Detect a new day
+export const detectNewDay = () => {
+  // Store the current time
+  execute.store.result.score(timeOfDay).run.time.query("daytime");
+
+  // Detect if tick of the day matches 36 and advance the day count
+  _.if(timeOfDay.equalTo(36), () => {
+    dayNumberArr.forEach((day, index) => {
+      _.if(daysPassed.equalTo(day), () => {
+        const nextIdx = index == dayNumberArr.length - 1 ? index : index + 1;
+        daysPassed.set(dayNumberArr[nextIdx]);
+      });
+    });
+    // daysPassed.add(1);
+    title("@a").title([
+      { text: "Day: ", color: "red" },
+      { score: { name: daysPassed.target, objective: daysPassed.objective.name }, color: "gold" },
+    ]);
+  });
+};
+
+// Reset the day count
+MCFunction("reset_day_count", () => {
+  daysPassed.set(0);
+});
