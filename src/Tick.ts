691eecb287df78d58d57b59f414003bb263492fa
Mizab <mizabmansari@gmail.com>
2024-09-07 23:46:14 +0530

    Added player pos score

--- src/Tick.ts
+++ src/Tick.ts
@@ -1,5 +1,6 @@
 import { MCFunction, Objective, team } from "sandstone";
 import { detectNewDay, speedUpDayClock } from "./Game/DayCycleController";
+import { updatePlayerPos } from "./Game/EnableWorldBorder";
 
 // *  Scoreboard
 // Scores that shows the stats of the game
@@ -11,12 +12,17 @@ export const daysPassed = sidebarScores(dayDisplay);
 const privateScoreboard = Objective.create("pvt_scoreboard", "dummy");
 export const timeOfDay = privateScoreboard("time_of_day");
 
+// Player position scoreboard
+export const playerPosX = Objective.create("pos_x", "dummy")("@s");
+export const playerPosZ = Objective.create("pos_z", "dummy")("@s");
+
 // ! Tick Function
 MCFunction(
   "tick",
   () => {
     speedUpDayClock();
     detectNewDay();
+    updatePlayerPos();
   },
   { runEachTick: true }
 );
