aa0fcfd69666743914b5c56ec249fe9c05c3c88a
Mizab <mizabmansari@gmail.com>
2024-09-07 23:45:58 +0530

    Added world border constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,2 +1,4 @@
 // Used this formula: (20 * 60 * 20) / (4 * 60 * 20), Days length = 24000 T
 export const TIME_TICKS = 5;
+export const WORLD_BORDER = 122;
+export const WORLD_BORDER_VISIBLE_DISTANCE = 22;
