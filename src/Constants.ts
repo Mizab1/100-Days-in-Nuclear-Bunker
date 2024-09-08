13554ad5741d79f9cec2372eac2a245bfb69fe66
Mizab <mizabmansari@gmail.com>
2024-09-08 13:10:56 +0530

    Added offset and center coords

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,10 @@
+import { abs, ColumnCoordinates } from "sandstone";
+
 // Used this formula: (20 * 60 * 20) / (4 * 60 * 20), Days length = 24000 T
 export const TIME_TICKS = 5;
+
 export const WORLD_BORDER_RADIUS = 1536;
 export const WORLD_BORDER_VISIBLE_DISTANCE = 30;
+export const WORLD_BORDER_X_OFFSET = 510;
+
+export const WORLD_BORDER_CENTER: ColumnCoordinates = abs(510, 0);
