9baf5cf2821b1c9ec34e2281c47e3ad5c8566e8b
Mizab <mizabmansari@gmail.com>
2024-09-11 22:32:45 +0530

    Added constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
-import { abs, ColumnCoordinates } from "sandstone";
+import { abs, ColumnCoordinates, Coordinates } from "sandstone";
 
 // Used this formula: (20 * 60 * 20) / (4 * 60 * 20), Days length = 24000 T
 export const TIME_TICKS = 5;
@@ -8,3 +8,7 @@ export const WORLD_BORDER_VISIBLE_DISTANCE = 30;
 export const WORLD_BORDER_X_OFFSET = 510;
 
 export const WORLD_BORDER_CENTER: ColumnCoordinates = abs(510, 0);
+
+export const ENEMY_COUNT = 5;
+
+export const BUNKER_COORDS: Coordinates = abs(-359, 79, 97);
