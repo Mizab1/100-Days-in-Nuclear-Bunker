4978389b97f1d41eb94784941de52bdcd7dac6fa
Mizab <mizabmansari@gmail.com>
2024-09-11 15:10:02 +0530

    Init

---
+++ src/Game/Enemy/Enemy.ts
@@ -0,0 +1,47 @@
+const easyEnemyArr = [
+  "minecraft:husk",
+  "minecraft:zombie",
+  "minecraft:creeper",
+  "minecraft:spider",
+  "minecraft:skeleton",
+  "minecraft:stray",
+  "minecraft:cave_spider",
+];
+
+const mediumEnemyArr = [
+  "mozombies_wave:nether_zombie",
+  "mozombies_wave:zombie_chef",
+  "mozombies_wave:zombie_creeper",
+  "mozombies_wave:zombie_cyborg",
+  "mozombies_wave:zombie_dwarf",
+  "mozombies_wave:zombie_king",
+  "mozombies_wave:zombie_knight",
+  "mozombies_wave:zombie_miner",
+  "mozombies_wave:zombie_pa",
+  "mozombies_wave:zombie_pirate",
+];
+
+const difficultEnemyArr = [
+  "prionmod:boomer",
+  "prionmod:cannibal",
+  "prionmod:crawler",
+  "prionmod:fhuman",
+  "prionmod:hellhound",
+  "prionmod:lacerator",
+  "prionmod:necro",
+  "prionmod:nephthys",
+  "prionmod:nightwalker",
+  "prionmod:rooted",
+  "prionmod:slammer",
+  "prionmod:slasher",
+  "prionmod:spitter",
+  "prionmod:starved",
+  "prionmod:wendigo",
+];
+
+const mutantEnemyArr = [
+  "mutantmonsters:mutant_zombie",
+  "mutantmonsters:mutant_skeleton",
+  "mutantmonsters:mutant_creeper",
+  "mutantmonsters:spider_pig",
+];
