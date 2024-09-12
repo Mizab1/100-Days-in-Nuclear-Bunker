4deaefc2c6c1b1caf7166d6887013e341806a23b
Mizab <mizabmansari@gmail.com>
2024-09-12 16:02:09 +0530

    Added a catcher and a limiter

--- src/Game/DayCycleController.ts
+++ src/Game/DayCycleController.ts
@@ -1,33 +1,55 @@
-import { _, execute, MCFunction, time, title } from "sandstone";
+import { _, execute, MCFunction, playsound, scoreboard, time, title, Variable } from "sandstone";
 import { TIME_TICKS } from "../Constants";
-import { daysPassed, timeOfDay } from "../Tick";
+import { daysPassed, isStarted, self, timeOfDay } from "../Tick";
 
-const dayNumberArr = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const dayNumberArr = [0, 1, 8, 15, 26, 31, 40, 58, 62, 75, 84, 97, 100];
 
 // Speed up the day, called by Tick function
-export const speedUpDayClock = () => {
+export const speedUpDayClock = MCFunction("day_controller/speed_up_day_clock", () => {
   time.add(TIME_TICKS);
-};
+});
 
 // Detect a new day
-export const detectNewDay = () => {
+export const detectNewDay = MCFunction("day_controller/detect_a_new_day", () => {
   // Store the current time
   execute.store.result.score(timeOfDay).run.time.query("daytime");
 
   // Detect if tick of the day matches 36 and advance the day count
   _.if(timeOfDay.equalTo(36), () => {
+    const catcher = Variable(0);
     dayNumberArr.forEach((day, index) => {
       if (index == dayNumberArr.length - 1) {
         return null;
       }
-      _.if(daysPassed.equalTo(day), () => {
+      _.if(_.and(daysPassed.equalTo(day), catcher.equalTo(0)), () => {
         daysPassed.set(dayNumberArr[index + 1]);
+        catcher.set(1);
       });
     });
+    // Completed the challenge of 100 days
+    _.if(_.and(daysPassed.equalTo(100), catcher.equalTo(0)), () => {
+      endChallenge();
+      catcher.set(1);
+    });
+
     // daysPassed.add(1);
     title("@a").title([
       { text: "Day: ", color: "red" },
       { score: { name: daysPassed.target, objective: daysPassed.objective.name }, color: "gold" },
     ]);
   });
-};
+});
+
+const endChallenge = MCFunction("day_controller/end_challenge", () => {
+  scoreboard.objectives.setDisplay("sidebar");
+  daysPassed.set(0);
+  isStarted.set(0);
+
+  execute
+    .as("@a")
+    .at(self)
+    .run(() => {
+      title(self).title({ text: "You have completed 100 Days!", color: "gold" });
+      playsound("minecraft:ui.toast.challenge_complete", "master", self);
+    });
+});
