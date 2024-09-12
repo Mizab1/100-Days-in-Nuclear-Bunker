bf9cf8fb22c8a9045e80e50d0a6cd428fd9ab994
Mizab <mizabmansari@gmail.com>
2024-09-12 16:02:25 +0530

    Fixed day count

--- src/Game/Start.ts
+++ src/Game/Start.ts
@@ -8,16 +8,19 @@ import {
   particle,
   playsound,
   rel,
+  scoreboard,
   Selector,
   sleep,
   spawnpoint,
   summon,
   teleport,
+  tellraw,
+  time,
   title,
   Variable,
 } from "sandstone";
 import { setScreenShakeTimer, shakeScreen } from "../Utils/ScreenShake";
-import { self } from "../Tick";
+import { daysPassed, isStarted, self, sidebarScores } from "../Tick";
 import { BUNKER_COORDS } from "../Constants";
 
 const shakeTime = 200;
@@ -25,6 +28,9 @@ const showExplosionScore = Variable(0);
 
 MCFunction("game/start", async () => {
   teleport("@a", abs(-1140, 97, -62), abs(22, -15));
+
+  // Reset the timings of the title
+  title("@a").reset();
 
   await sleep("20t");
 
@@ -49,7 +55,6 @@ MCFunction("game/start", async () => {
 
   await sleep(shakeTime);
   showExplosionScore.set(0);
-  kill(Selector("@e", { type: "block_display", tag: "dummy_bunker_marker" }));
 });
 
 MCFunction(
@@ -80,8 +85,21 @@ MCFunction(
         // Reset the score
         showExplosionScore.set(0);
 
+        // Set the scores
+        // isStarted.set(1);
+        daysPassed.set(0);
+
+        // Set the time to morning
+        time.set("0t");
+
+        // Enable the sidebar
+        scoreboard.objectives.setDisplay("sidebar", sidebarScores.name);
+
+        // Kill the glowing block
+        kill(Selector("@e", { type: "block_display", tag: "dummy_bunker_marker" }));
+
         // Teleport the player to the real Bunker
-        title("@a").title({ text: "You are in the Bunker", color: "green" });
+        tellraw("@a", { text: "You are in the Bunker", color: "green" });
         teleport("@a", BUNKER_COORDS);
         playsound("minecraft:block.note_block.bell", "master", "@a", BUNKER_COORDS);
         spawnpoint("@a", BUNKER_COORDS);
