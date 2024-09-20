3c605ec9d9221ec7a04d3269e6a60f712b4994e0
Mizab <mizabmansari@gmail.com>
2024-09-20 19:11:51 +0530

    Changed the entry sequence

--- src/Game/Start.ts
+++ src/Game/Start.ts
@@ -1,6 +1,8 @@
 import {
   _,
   abs,
+  Coordinates,
+  effect,
   execute,
   kill,
   MCFunction,
@@ -12,6 +14,7 @@ import {
   Selector,
   sleep,
   spawnpoint,
+  stopsound,
   summon,
   teleport,
   tellraw,
@@ -23,11 +26,22 @@ import { setScreenShakeTimer, shakeScreen } from "../Utils/ScreenShake";
 import { daysPassed, isStarted, self, sidebarScores } from "../Tick";
 import { BUNKER_COORDS } from "../Constants";
 
-const shakeTime = 200;
-const showExplosionScore = Variable(0);
+const shakeTime = 20 * 25;
+
+const glowingMarkerCoords = [
+  abs(-1121, 99, -55),
+  abs(-1139, 99, -47),
+  abs(-1139, 99, -28),
+  abs(-1159, 99, -19),
+  abs(-1183, 99, -20),
+];
 
 MCFunction("game/start", async () => {
-  teleport("@a", abs(-1140, 97, -62), abs(22, -15));
+  // Teleport the player near the explosion
+  teleport("@a", abs(-1211, 98, -12), abs(5, -16));
+
+  // Play the nuclear explosion sound
+  execute.as("@a").at(self).run.playsound("minecraft:sfx.nuclear_explosion", "master", self);
 
   // Reset the timings of the title
   title("@a").reset();
@@ -36,44 +50,12 @@ MCFunction("game/start", async () => {
 
   title("@a").title({ text: "Go in the Bunker for shelter", color: "red" });
   title("@a").subtitle({ text: "Follow the Glowing Marker", color: "gold" });
-  summon("block_display", abs(-1121, 99, -55), {
-    Glowing: NBT.byte(1),
-    Tags: ["dummy_bunker_marker"],
-    transformation: {
-      left_rotation: [NBT.float(0), NBT.float(0), NBT.float(0), NBT.float(1)],
-      right_rotation: [NBT.float(0), NBT.float(0), NBT.float(0), NBT.float(1)],
-      translation: [NBT.float(-0.25), NBT.float(-0.25), NBT.float(-0.25)],
-      scale: [NBT.float(0.5), NBT.float(0.5), NBT.float(0.5)],
-    },
-    block_state: { Name: "minecraft:gold_block" },
-  });
+  glowingMarkerCoords.forEach((coord) => placeGlowingMarker(coord));
 
   await sleep("10t");
 
   setScreenShakeTimer(shakeTime);
-  showExplosionScore.set(1);
-
-  await sleep(shakeTime);
-  showExplosionScore.set(0);
 });
-
-MCFunction(
-  "game/start/show_explosion",
-  () => {
-    _.if(showExplosionScore["=="](1), () => {
-      execute
-        .as("@a")
-        .at(self)
-        .run(() => {
-          particle("minecraft:explosion", rel(0, 0, 0), [2, 2, 2], 0, 3);
-          playsound("minecraft:entity.generic.explode", "master", self);
-        });
-    });
-  },
-  {
-    runEach: "10t",
-  }
-);
 
 MCFunction(
   "game/start/detect_player",
@@ -82,9 +64,6 @@ MCFunction(
       .positioned(abs(-1116, 96, -55))
       .if.entity(Selector("@a", { distance: [Infinity, 1.5] }))
       .run(() => {
-        // Reset the score
-        showExplosionScore.set(0);
-
         // Set the scores
         isStarted.set(1);
         daysPassed.set(0);
@@ -100,6 +79,8 @@ MCFunction(
 
         // Teleport the player to the real Bunker
         tellraw("@a", { text: "You are in the Bunker", color: "green" });
+        stopsound("@a");
+        effect.give("@a", "minecraft:blindness", 4, 0, true);
         teleport("@a", BUNKER_COORDS);
         playsound("minecraft:block.note_block.bell", "master", "@a", BUNKER_COORDS);
         spawnpoint("@a", BUNKER_COORDS);
@@ -117,4 +98,18 @@ export const screenShakeTick = () => {
     .run(() => {
       shakeScreen(shakeTime, [3, 3]);
     });
+};
+
+const placeGlowingMarker = (coord: Coordinates) => {
+  summon("block_display", coord, {
+    Glowing: NBT.byte(1),
+    Tags: ["dummy_bunker_marker"],
+    transformation: {
+      left_rotation: [NBT.float(0), NBT.float(0), NBT.float(0), NBT.float(1)],
+      right_rotation: [NBT.float(0), NBT.float(0), NBT.float(0), NBT.float(1)],
+      translation: [NBT.float(-0.25), NBT.float(-0.25), NBT.float(-0.25)],
+      scale: [NBT.float(0.5), NBT.float(0.5), NBT.float(0.5)],
+    },
+    block_state: { Name: "minecraft:gold_block" },
+  });
 };
